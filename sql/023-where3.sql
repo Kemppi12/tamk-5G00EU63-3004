310108ec1355b963f50f4845f06c521f7e06a3ff
Timo Kemppinen <timo.kemppinen@tuni.fi>
2024-01-16 19:58:12 +0200

    sql/023: add

---
+++ sql/023-where3.sql
@@ -0,0 +1,10 @@
+-- Author: Timo Kemppinen <timo.kemppinen@tuni.fi>
+-- Date: 2024-16-01
+-- File: 023-where3.sql
+
+SELECT ename, sal
+FROM   emp
+WHERE  (sal < 1500 OR sal > 2850)
+;
+
+-- End of file
