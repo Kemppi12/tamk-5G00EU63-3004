8c7ed259c0d4543f21c21e0d0fc17583c2bdd75b
Timo Kemppinen <timo.kemppinen@tuni.fi>
2024-01-16 19:15:31 +0200

    sql/013: add

---
+++ sql/013-query1.sql
@@ -0,0 +1,12 @@
+-- Author: Timo Kemppinen <timo.kemppinen@tuni.fi>
+-- Date: 2024-16-01
+-- File: 013-query1.sql
+
+SELECT ename
+       , job
+       , hiredate
+       , empno
+FROM   emp
+;
+
+-- End of file
