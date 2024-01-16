6524ecdb942a0599b5153d4c1a7de55884f1b7af
Timo Kemppinen <timo.kemppinen@tuni.fi>
2024-01-16 19:34:55 +0200

    sql/015: add

---
+++ sql/015-query3.sql
@@ -0,0 +1,12 @@
+-- Author: Timo Kemppinen <timo.kemppinen@tuni.fi>
+-- Date: 2024-16-01
+-- File: 015-query3.sql
+
+SELECT job
+FROM   emp
+GROUP BY  job
+HAVING COUNT(*) = 1
+ORDER BY job ASC;
+;
+
+-- End of file
