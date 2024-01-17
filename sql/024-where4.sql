14459ffa78352ca9fd4516c51595c2d14c0fe63c
Timo Kemppinen <timo.kemppinen@tuni.fi>
2024-01-17 13:25:05 +0200

    sql/024: correct

---
+++ sql/024-where4.sql
@@ -0,0 +1,10 @@
+-- Author: Timo Kemppinen <timo.kemppinen@tuni.fi>
+-- Date: 2024-01-17
+-- File: 024-where4.sql
+
+SELECT ename, job, hiredate
+FROM   emp
+WHERE  ( hiredate >= 1981-02-20 AND hiredate <= 1981-05-1981)
+;
+
+-- End of file
