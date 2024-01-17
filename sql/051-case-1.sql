660ae28293432452cc0779017fd999cb4b594d34
Timo Kemppinen <timo.kemppinen@tuni.fi>
2024-01-17 19:51:51 +0200

    sql/051: add

---
+++ sql/051-case-1.sql
@@ -0,0 +1,13 @@
+-- Author: Timo Kemppinen <timo.kemppinen@tuni.fi>
+-- Date: 2024-01-17
+-- File: 051-case1.sql
+
+UPDATE emp
+SET job = 'SALES PERSON'
+WHERE job = 'SALESMAN';
+
+SELECT ename, job
+FROM emp
+ORDER BY job ASC, ename ASC;
+
+-- End of file
