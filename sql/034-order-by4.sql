2caf44a59ed2ccd353cd28179dd7235e608036ed
Timo Kemppinen <timo.kemppinen@tuni.fi>
2024-01-17 20:29:02 +0200

    sql/034: add

---
+++ sql/034-order-by4.sql
@@ -0,0 +1,13 @@
+-- Author: Timo Kemppinen <timo.kemppinen@tuni.fi>
+-- Date: 2024-01-17
+-- File: 034-order-by4.sql
+
+SELECT ename AS 'Employee',
+       sal AS 'Monthly Salary',
+       sal * 1.10 AS 'Sal 10%',
+       comm * 1.10 AS 'Comm'
+FROM emp
+WHERE comm > sal * 1.10
+ORDER BY ename ASC, sal ASC, comm ASC;
+
+-- End of file
