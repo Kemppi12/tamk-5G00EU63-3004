a32d8492933a976c11eed782593a16eb1854f1bc
Timo Kemppinen <timo.kemppinen@tuni.fi>
2024-01-17 13:06:17 +0200

    sql/033: add

---
+++ sql/033-order-by3.sql
@@ -0,0 +1,17 @@
+-- Author: Timo Kemppinen <timo.kemppinen@tuni.fi>
+-- Date: 2024-01-17
+-- File: 033-order-by3.sql
+
+SELECT deptno
+        AS 'Dept',
+        ename
+        AS 'Employee',
+        sal * 0.31
+        AS Taxes
+FROM emp
+WHERE deptno = 10
+ORDER BY deptno ASC,
+ename ASC
+;
+
+-- End of file
