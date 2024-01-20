6218b6fb6f37d1dbb84da99460bc8d45f42ab044
Timo Kemppinen <timo.kemppinen@tuni.fi>
2024-01-20 09:19:40 +0200

    sql/082: add

---
+++ sql/082-group-functions2.sql
@@ -0,0 +1,10 @@
+-- Author: Timo Kemppinen <timo.kemppinen@tuni.fi>
+-- Date: 2024-01-20
+-- File: 082-group-functions2.sql
+
+
+SELECT COUNT(*) AS 'count of dept 30'
+FROM emp
+WHERE deptno = 30;
+
+-- End of file
