9c714054f0fd22712bd54fc64ca254d8d8087f1b
Timo Kemppinen <timo.kemppinen@tuni.fi>
2024-01-20 09:30:31 +0200

    sql/083: add

---
+++ sql/083-qroup-functions3.sql
@@ -0,0 +1,11 @@
+-- Author: Timo Kemppinen <timo.kemppinen@tuni.fi>
+-- Date: 2024-01-20
+-- File: 083-group-functions3.sql
+
+
+SELECT MAX (sal) AS 'high',
+        MIN (sal) AS 'low'
+FROM emp
+WHERE deptno = 30 OR deptno = 10;
+
+-- End of file
