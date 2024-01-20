a6b3625db1b30d034fa1ba8b8bd8475247471b5d
Timo Kemppinen <timo.kemppinen@tuni.fi>
2024-01-20 09:03:45 +0200

    sql/072: add

---
+++ sql/072-functions2.sql
@@ -0,0 +1,12 @@
+-- Author: Timo Kemppinen <timo.kemppinen@tuni.fi>
+-- Date: 2024-01-20
+-- File: 072-functions2.sql
+
+
+SELECT  ename AS 'Name',
+LENGTH (ename) AS 'Characters'
+FROM emp
+ORDER BY LENGTH (ename) ASC, ename ASC
+;
+
+-- End of file
