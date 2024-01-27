8d16335f7bbbd23c6a28ddcc7a0707d24c5bf782
Timo Kemppinen <timo.kemppinen@tuni.fi>
2024-01-27 18:58:40 +0200

    sql/052: correct

--- sql/052-case-2.sql
+++ sql/052-case-2.sql
@@ -2,19 +2,14 @@
 -- Date: 2024-01-17
 -- File: 052-case2.sql
 
-UPDATE emp
-SET comm = comm * 1.05
-WHERE deptno = 30
-;
-
-UPDATE emp
-SET comm = 100
-WHERE comm IS NULL
-AND deptno = 30
-;
-
-SELECT ename, comm
-FROM emp
-ORDER BY ename ASC, comm ASC;
+SELECT   ename,
+  CASE
+    WHEN deptno = 30 THEN COALESCE(comm * 1.05, 100)
+    ELSE comm
+  END AS comm
+FROM
+  emp
+ORDER BY
+  ename ASC, comm ASC;
 
 -- End of file
