eef717913512cd963a183bf654710af636d02096
Timo Kemppinen <timo.kemppinen@tuni.fi>
2024-01-27 15:50:44 +0200

    sql/111: correct

--- sql/111-create-table1.sql
+++ sql/111-create-table1.sql
@@ -5,12 +5,12 @@
 
 CREATE TABLE person (
     id INTEGER PRIMARY KEY NOT NULL,
-    last_name AS 'last' VARCHAR(50) NOT NULL,
-    first_name AS 'first' VARCHAR(50) NOT NULL,
+    last VARCHAR(50) NOT NULL,
+    first VARCHAR(50) NOT NULL,
     phone VARCHAR(20) CHECK (phone LIKE '+%'),
     zip VARCHAR(10),
     city VARCHAR(50),
-    street_address AS 'address' VARCHAR(255)
+    address VARCHAR(255)
 );
 
 -- End of file
