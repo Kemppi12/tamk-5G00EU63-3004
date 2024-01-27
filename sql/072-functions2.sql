9c229f267f02a2f3fd397bd3276ce0e5ea22200f
Timo Kemppinen <timo.kemppinen@tuni.fi>
2024-01-27 17:14:15 +0200

    sql/072: correct

--- sql/072-functions2.sql
+++ sql/072-functions2.sql
@@ -3,10 +3,10 @@
 -- File: 072-functions2.sql
 
 
-SELECT  ename AS 'Name',
-LENGTH(UPPER(ename)) AS 'Characters'
+SELECT ename AS 'Name',
+LENGTH(LOWER(ename)) AS 'Characters'
 FROM emp
-ORDER BY LENGTH(UPPER(ename)) ASC, UPPER(ename) ASC;
+ORDER BY LENGTH(LOWER(ename)) ASC, LOWER(ename) ASC;
 ;
 
 -- End of file
