50891aa0d965b0841b82aa6c08dc848ab35705b9
Timo Kemppinen <timo.kemppinen@tuni.fi>
2024-01-27 16:03:00 +0200

    sql/113: add

---
+++ sql/113-create-table3.sql
@@ -0,0 +1,15 @@
+-- Author: Timo Kemppinen <timo.kemppinen@tuni.fi>
+-- Date: 2024-01-27
+-- File: create-table3.sql
+
+
+CREATE TABLE weather (
+    id INTEGER PRIMARY KEY NOT NULL,
+    time_of_reading VARCHAR(50) NOT NULL,
+    high INTEGER NOT NULL,
+    low INTEGER NOT NULL,
+    signature VARCHAR(50),
+    comment VARCHAR(50),
+);
+
+-- End of file
