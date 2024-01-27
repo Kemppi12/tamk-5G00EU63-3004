13cb935877df1250673dbe052ad9fb39d1bd694e
Timo Kemppinen <timo.kemppinen@tuni.fi>
2024-01-27 14:44:02 +0200

    sql/094: add

---
+++ sql/094-multiple-tables4.sql
@@ -0,0 +1,16 @@
+-- Author: Timo Kemppinen <timo.kemppinen@tuni.fi>
+-- Date: 2024-01-27
+-- File: multiple-tables4.sql
+
+
+SELECT  emp.ename,
+        dept.dname,
+        dept.loc
+FROM    emp
+JOIN    dept ON emp.deptno = dept.deptno
+WHERE   emp.ename LIKE '%a%'
+AND emp.sal > 1100 AND emp.sal <= 2200
+ORDER BY emp.ename ASC
+;
+
+-- End of file
