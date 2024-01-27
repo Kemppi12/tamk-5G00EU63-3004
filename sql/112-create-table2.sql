4374e5ff324844e20c04e90cc7b028f46b513b3e
Timo Kemppinen <timo.kemppinen@tuni.fi>
2024-01-27 15:58:22 +0200

    sql/112: add

---
+++ sql/112-create-table2.sql
@@ -0,0 +1,14 @@
+-- Author: Timo Kemppinen <timo.kemppinen@tuni.fi>
+-- Date: 2024-01-27
+-- File: create-table2.sql
+
+
+CREATE TABLE football (
+    id INTEGER PRIMARY KEY NOT NULL,
+    'first' VARCHAR(50) NOT NULL,
+    'last' VARCHAR(50) NOT NULL,
+    team VARCHAR(50),
+    comment VARCHAR(50),
+);
+
+-- End of file
