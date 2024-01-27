1a768b05ebe73c9adacdc7e64d0847c742938fd8
Timo Kemppinen <timo.kemppinen@tuni.fi>
2024-01-27 14:59:27 +0200

    sql/111: add

---
+++ sql/111-create-table1.sql
@@ -0,0 +1,16 @@
+-- Author: Timo Kemppinen <timo.kemppinen@tuni.fi>
+-- Date: 2024-01-27
+-- File: create-table1.sql
+
+
+CREATE TABLE person (
+    id INTEGER PRIMARY KEY,
+    last_name AS 'last' VARCHAR(50) NOT NULL,
+    first_name AS 'first' VARCHAR(50) NOT NULL,
+    phone VARCHAR(20) CHECK (phone LIKE '+%'),
+    zip VARCHAR(10),
+    city VARCHAR(50),
+    street_address AS 'address' VARCHAR(255)
+);
+
+-- End of file
