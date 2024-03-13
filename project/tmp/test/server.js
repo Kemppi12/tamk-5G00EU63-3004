f1d7170f1168386f17abc7a5869cda751abe4c85
Timo Kemppinen <timo.kemppinen@tuni.fi>
2024-03-13 19:35:45 +0200

    init project

---
+++ project/tmp/test/server.js
@@ -0,0 +1,51 @@
+// FILE: server.js
+    //
+    // EXAMPLE CALL
+    //
+    // node server.js
+    // curl 'http://127.0.0.1:8000/app.js?a=1&b=2'
+
+    var http = require("http")
+    var url = require('url')
+    var qs = require('querystring');
+    var util = require('util')
+
+    var hostname = "127.0.0.1"
+    var port = 8000
+
+    var server = http.createServer( (req, res) => {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+
+        //   req.url => /app.js?a=1&b=2
+        res.write("request URL: " + req.url + '\n');
+
+        // ---------------------------------------------------
+        // read and parse the request
+
+        let q = url.parse(req.url, true);  // returns an URL object
+        console.log("DATA query URL object: " + q);
+        console.log(util.inspect(q, {showHidden: false, depth: null}));
+
+        // ---------------------------------------------------
+        // COMMON VARIABLES: see documentation for module 'url'
+        //
+        // .host      localhost:8080   WARN: can be <null>
+        // .pathname  /server.js
+        // .search    ?a=1&b=2
+        // .query     { key: val, key: val }
+
+        res.write("query.pathname: " + q.pathname + '\n');
+        res.write("query.search: " + q.search + '\n');
+
+        console.log("DATA q.query", q.query);
+        let a = q.query.a;
+        let b = q.query["b"];
+        res.write("Param a: " + a + '\n');
+        res.write("Param b: " + b + '\n');
+
+        res.end('Response end\n');
+    })
+
+    server.listen(port, hostname, () => {
+        console.log(`Server running at http://${hostname}:${port}/`);
+    })
